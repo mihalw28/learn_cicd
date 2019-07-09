37cfc7fe47374192fce3cfb6f0d4eae9de3431d3
mihalw28 <michal.waszak@yahoo.com>
2019-07-09 10:53:55 +0200

    Turn program into a web app

---
+++ app.py
@@ -0,0 +1,18 @@
+import os
+from flask import Flask
+from buzz import generator
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def generate_buzz():
+    page = "<html><body><h1>"
+    page += generator.generate_buzz()
+    page += "</h1></body></html>"
+    return page
+
+
+if __name__ == "__main__":
+    app.run()
